8e39345639d9c3238a3b76e986bf8a7e2eafaacc
GajananDhangude <dhangudegajanan27@gmail.com>
2026-01-06 21:42:05 +0530

    FAISS as a vectore store.

---
+++ backend/app/vectorestore.py
@@ -0,0 +1,22 @@
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+class VectoreStore():
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.index = None
+        self.documents = []
+
+    def add(self , docs):
+        texts = [d['content'] for d in docs]
+        embeddings = self.model.encode(texts)
+
+        self.index = faiss.IndexFlatL2(embeddings.shape[1])
+        self.index.add(np.array(embeddings))
+        self.documents = docs
+
+    def search(self , query , k=5):
+        query_enb = self.model.encode([query])
+        _ , idxs = self.index.search(np.array(query_enb) , k)
+        return [self.documents[i] for i in idxs[0]]
