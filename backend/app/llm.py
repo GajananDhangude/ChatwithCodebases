02ba4d465515050a63f36d73bc1d558dcc4df8a9
GajananDhangude <dhangudegajanan27@gmail.com>
2026-01-07 09:08:41 +0530

    integrated a large language model gpt opensource

---
+++ backend/app/llm.py
@@ -0,0 +1,18 @@
+
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0.2,
+    max_tokens=1000,
+    max_retries=2
+)
+
+
+if __name__ == "__main__":
+    message = llm.invoke("The First person to go to moon was")
+    print(message)
