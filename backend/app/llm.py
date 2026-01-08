424c9de8e69d1b06f4f6a9544e166a6077fcb4d6
GajananDhangude <dhangudegajanan27@gmail.com>
2026-01-08 09:34:01 +0530

    integrated large language model

--- backend/app/llm.py
+++ backend/app/llm.py
@@ -7,12 +7,16 @@ load_dotenv()
 
 llm = ChatGroq(
     model="openai/gpt-oss-120b",
-    temperature=0.2,
+    temperature=0.7,
     max_tokens=1000,
     max_retries=2
 )
 
 
-if __name__ == "__main__":
-    message = llm.invoke("The First person to go to moon was")
-    print(message)
+
+
+
+
+# if __name__ == "__main__":
+#     message = llm.invoke("The First person to go to moon was")
+#     print(message)
